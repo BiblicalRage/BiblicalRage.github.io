4cd7574ee2c59a5262e52132d0129a393088d16e
BiblicalRage <Jeff_Levesque@rocketmail.com>
2025-06-22 11:05:36 -0500

    Change base path to root for simpler URL structure

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BiblicalRage.github.io/',
+  base: '/',
   server: {
     port: 5173,
     host: true,
